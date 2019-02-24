c2698b7b931c9577a8382f0832afbe239026751e
zhouchangxun <changxunzhou@qq.com>
2019-02-24 14:12:46 +0800

    update argv 2

--- wsgi.py
+++ wsgi.py
@@ -3,12 +3,12 @@
 import websockify
 import sys,os
 
-telnetd = '/home/leanapp/app/telnetd'
+telnetd = '/home/leanengine/app/telnetd'
 cmd = 'nohup '+telnetd+' &'
 print '[run] ' + cmd
 os.system(cmd)
 
-sys.argv = sys.argv + ['--web','./','-D','3000','localhost:7777']
+sys.argv = [sys.argv[0],'--web','./','-D','3000','localhost:7777']
 print sys.argv
 
 websockify.websocketproxy.websockify_init()
