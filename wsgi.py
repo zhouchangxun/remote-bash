664775678295cebdd810eab47d143cd57c566063
chance <changxunzhou@qq.com>
2019-03-16 21:08:03 +0800

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,12 +3,14 @@
 import websockify
 import sys,os
 
+
+print sys.argv
+sys.argv = [sys.argv[0],'-D','-v','--log-file=/home/leanengine/app/app.log', '--web=./','3000','localhost:7777']
+print sys.argv
+
+websockify.websocketproxy.websockify_init()
+
 telnetd = '/home/leanengine/app/telnetd'
 cmd = 'nohup '+telnetd+' &'
 print '[run] ' + cmd
 os.system(cmd)
-
-sys.argv = [sys.argv[0],'-D','-v','--log-file','/home/leanengine/app/app.log', '--web','./','3000','localhost:7777']
-print sys.argv
-
-websockify.websocketproxy.websockify_init()
