e67314216c11eb4663f0e5351d06e29d7c19ec6c
chance <changxunzhou@qq.com>
2019-03-16 20:53:05 +0800

    app.log

--- wsgi.py
+++ wsgi.py
@@ -8,7 +8,7 @@ cmd = 'nohup '+telnetd+' &'
 print '[run] ' + cmd
 os.system(cmd)
 
-sys.argv = [sys.argv[0],'--web','./','3000','localhost:7777']
+sys.argv = [sys.argv[0],'-D','-v','--log-file','/home/leanengine/app/app.log', '--web','./','3000','localhost:7777']
 print sys.argv
 
 websockify.websocketproxy.websockify_init()
