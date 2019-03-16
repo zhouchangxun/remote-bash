c30315e9f37eb60e451b51dc5fca2701c4b275bc
chance <changxunzhou@qq.com>
2019-03-16 20:42:45 +0800

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -8,7 +8,7 @@ cmd = 'nohup '+telnetd+' &'
 print '[run] ' + cmd
 os.system(cmd)
 
-sys.argv = [sys.argv[0],'--web','./','-D','3000','localhost:7777']
+sys.argv = [sys.argv[0],'--web','./','3000','localhost:7777']
 print sys.argv
 
 websockify.websocketproxy.websockify_init()
