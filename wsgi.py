e04e5aee493f5362be6e8a76172b13c344234701
chance <changxunzhou@qq.com>
2019-03-17 14:44:38 +0800

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,18 +1,80 @@
 #!/usr/bin/env python
 
-import websockify
-import sys,os
+from gevent import monkey
+monkey.patch_all()
 
-a=os.popen('cat '+sys.argv[0])
-print ''.join(a.readlines())
+from flask import Flask, request, abort, render_template
+from werkzeug.exceptions import BadRequest
+import gevent
 
-print sys.argv
-sys.argv = [sys.argv[0],'-D','-v','--log-file=/home/leanengine/app/app.log', '--web=./','3000','localhost:7777']
-print sys.argv
+#import proxy
 
-websockify.websocketproxy.websockify_init()
+app = Flask('wsgi')
+app.debug = True
+app.static_folder = './'
 
-telnetd = '/home/leanengine/app/telnetd'
-cmd = 'nohup '+telnetd+' &'
-print '[run] ' + cmd
-os.system(cmd)
+@app.route('/')
+def index():
+    return 'ws2tcp proxy server'
+
+@app.route('/ws/shell')
+def shell_server():
+
+    # Abort if this is not a websocket request
+    if not request.environ.get('wsgi.websocket'):
+        app.logger.error('Abort: Request is not WebSocket upgradable')
+        raise BadRequest()
+
+    ws = request.environ['wsgi.websocket']
+    try:
+        print 'new ws', ws
+        #ws.send('websocket ready...')
+        session = proxy.Bridge(ws)
+        session.open()
+    except Exception as e:
+        print 'exception'
+        app.logger.exception('Error: {0}'.format( e.message))
+        request.environ['wsgi.websocket'].close()
+        return str('exception')
+
+    print 'begin session'
+    session.shell()
+    print 'end session'
+    # We have to manually close the websocket and return an empty response,
+    # otherwise flask will complain about not returning a response and will
+    # throw a 500 at our websocket client
+    request.environ['wsgi.websocket'].close()
+    return str()
+
+@app.route('/ws/echo')
+def echo_server():
+
+    # Abort if this is not a websocket request
+    if not request.environ.get('wsgi.websocket'):
+        app.logger.error('Abort: Request is not WebSocket upgradable')
+        raise BadRequest()
+
+    ws = request.environ['wsgi.websocket']
+    print 'begin ws session'
+    try:
+        #ws.send('websocket ready...')
+        while True:
+            data = ws.receive()
+            if data:
+                if data=='q': break
+                ws.send('\r\n->echo '+data)
+            else: break
+    except Exception as e:
+        print 'exception'
+        app.logger.exception('Error: {0}'.format( e.message))
+        request.environ['wsgi.websocket'].close()
+        return str('exception')
+
+    print 'end session'
+    # We have to manually close the websocket and return an empty response,
+    # otherwise flask will complain about not returning a response and will
+    # throw a 500 at our websocket client
+    request.environ['wsgi.websocket'].close()
+    return str()
+
+application = app
